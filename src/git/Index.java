0079432b6bb74b9d770b38ac17857af085e7943a
Zach Rossen <zachary.rossen@gmail.com>
2022-09-28 12:08:16 -0700

    Day 9

--- src/git/Index.java
+++ src/git/Index.java
@@ -6,38 +6,41 @@ import java.io.IOException;
 import java.util.HashMap;
 
 public class Index {
-	FileWriter index; //table of contents, list on every signle item, and where it exists 
-	HashMap <String, Blob> objects; 
+	FileWriter index; // table of contents, list on every signle item, and where it exists
+	HashMap<String, Blob> objects;
+
 	public Index() throws IOException {
 		initialize();
-		
+
 	}
+
 	public void initialize() throws IOException {
-		index = new FileWriter("index"); 
-		objects = new HashMap <String,Blob> ();
+		index = new FileWriter("index");
+		objects = new HashMap<String, Blob>();
 		File theDir = new File("/path/objects");
-		if (!theDir.exists()){
-		    theDir.mkdirs();
+		if (!theDir.exists()) {
+			theDir.mkdirs();
 		}
 	}
+
 	public void addBlob(String fileName) throws IOException {
-		Blob newBlobWithFileName = new Blob (fileName);
-		objects.put(fileName,newBlobWithFileName);
-		index.append(fileName + " : " + newBlobWithFileName); 
+		Blob newBlobWithFileName = new Blob(fileName);
+		objects.put(fileName, newBlobWithFileName);
+		index.append(fileName + " : " + newBlobWithFileName);
 	}
-	
-	public void writeAllOfIndex () throws IOException {
-		for(String str : objects.keySet()) {
-			index.append(str + " : " ); 
-			index.append(" : " + objects.get(str).getSha());
+
+	public void writeAllOfIndex() throws IOException {
+		for (String str : objects.keySet()) {
+			index.append(str + " : ");
+			index.append(" : " + objects.get(str).getSha() + "txt.txt");
 		}
 	}
-	
-	 
+
 	public void removeBlob(String fileName) throws IOException {
 		objects.remove(fileName);
 		writeAllOfIndex();
-		FileWriter fileToDelete = new FileWriter("./objects/"+ objects.get(fileName).getSha());
-		fileToDelete.close(); 	}
-	
+		FileWriter fileToDelete = new FileWriter("./objects/" + objects.get(fileName).getSha());
+		fileToDelete.close();
+	}
+
 }
